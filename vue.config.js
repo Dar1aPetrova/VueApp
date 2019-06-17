f8b3c16d27ecbaedbb397a0cfb8ce413f33e0b05
Daria Petrova <dasha.nsu.1112@gmail.com>
2019-06-17 12:28:08 +0700

    deploy index

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://github.com/Dar1aPetrova/Dar1aPetrova.github.io/quiz-app-on-vue/'
+    ? '/quiz-app-on-vue/'
     : '/',
 };
